7a72f8cffe8a595c7faad9f8f997cab2c9d634d8
EvgeniyaNesterkova <miledieva08@gmail.com>
2023-12-14 19:37:15 +0300

    контроль пустой заметки

--- app.js
+++ app.js
@@ -12,8 +12,10 @@ const App = {
             this.inputValue = event.target.value;
         },
         addNewNote () {
-            this.notes.push(this.inputValue);
-            this.inputValue = '';
+            if (this.inputValue !== '') {
+                this.notes.push(this.inputValue);
+                this.inputValue = '';
+            }
         },
         removeNote(index) {
             this.notes.splice(index, 1);
