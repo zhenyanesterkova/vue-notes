a916a66bfea70db360946d75d56a1a4624b718b0
EvgeniyaNesterkova <miledieva08@gmail.com>
2023-12-14 19:35:32 +0300

    добавление и удаление заметок

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const App = {
     data() {
         return {
-            counter: 0,
-            title: 'Счетчик'
+            title: 'Список заметок',
+            placeholderString: 'Введите название заметки',
+            inputValue: '',
+            notes: ['Заметка 1', 'Замека 2']
+        }
+    },
+    methods: {
+        inputChangeHandler (event) {
+            this.inputValue = event.target.value;
+        },
+        addNewNote () {
+            this.notes.push(this.inputValue);
+            this.inputValue = '';
+        },
+        removeNote(index) {
+            this.notes.splice(index, 1);
+
         }
     }
-    
 }
 
 Vue.createApp(App).mount('#app');
